997d2999acc2deec31ab4679625121da66d2f630
Szilveszter98 <szilveszter.mag98@gmail.com>
2020-12-30 12:16:52 +0100

    some edits and new files

---
+++ createPost.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create Post</title>
+</head>
+<body>
+<center>
+<!-- Form to create new products -->
+<h1> Creating a new Post </h1>
+<form method="GET" action="v1/posts/addPost.php">
+            
+            Title:<input type="text" name="title" placeholder="Product Title" /><br />
+            Information:<textarea name="information" placeholder="Information" rows="1" cols="18"></textarea><br />
+            Date?
+            type of work...
+            <hr>
+            <input type="submit" name="Add"/>
+        </form>
+</center>
+
+<?php
+
+
+
+?>
+</body>
+</html> 
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+<!-- Main page and log in button and register button -->
+<center><h1> Welcome </h1> </br>
+<h2> To see the works or add work, you need to register </h2></br>
+
+
+Company / privatperson
+If company shpow 
+<a href="signupCompany.php"> Log in </a> </br>
+If pp show 
+<a href="signup.php"> Register </a> <center>
+<hr>
+</body>
+</html>
